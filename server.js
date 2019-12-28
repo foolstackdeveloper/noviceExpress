e0765de851297551f261471b30ee40c8e08d46ec
kitichai phadungnanon <kitichai.p@linecorp.com>
2019-12-28 14:29:07 +0700

    1st commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+//app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/dummypublic', express.static('public'))
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+app.post("/", function(req, res) {
+  res.send("Got a POST request");
+});
+
+app.put("/user", function(req, res) {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", function(req, res) {
+  res.send("Got a DELETE request at /user");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
